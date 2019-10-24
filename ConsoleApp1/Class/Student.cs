13b1b41356b4a8b2df37ed018e760c147cac28f9
Clinton Carter <clinton.carter2018@ou.edu>
2019-10-24 11:33:10 -0500

    participation 10-24

---
+++ ConsoleApp1/Class/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Student
+    {
+        private string name; // "-" = private, + == public, 
+        private int id;
+
+        public string city ( get; set; )
+          
+
+        public int getID()
+        {
+
+            return id;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public void setID(int newID)
+        {
+            id = newID;
+        }
+        public Student() //collector or constructor
+        {
+            id = 5;
+            name = string.Empty; //""
+        }
+
+    }
+}
