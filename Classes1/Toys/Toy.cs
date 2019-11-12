ba75c5d92e336dac1220ef5a15f326341c1053aa
Clinton Carter <clinton.carter2018@ou.edu>
2019-11-12 11:59:52 -0600

    commit

---
+++ Classes1/Toys/Toy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toys
+{
+    class Toy
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        private string Aisle { get; set; }
+    
+        public Toy()
+        {
+            Manufacturer = "";
+            Name = "";
+            Price = 0;
+            Aisle = "";
+
+              
+        }
+        public string GetAisle()
+        {
+
+            for (int i = 0; i < Manufacturer.Length; i++)
+            {
+                i = 0;
+                string manLetter = Manufacturer(i);
+            }
+            Random rand = new Random();
+            double randomNumber = rand.Next(1, 25);
+
+            return "" ;
+        }
+    }
+}
---
+++ Classes1/Toys/ToyBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toys
+{
+    class ToyBox
+    {
+        public List<Toy> = new List<Toy>();
+        
+       public ToyBox()
+        {
+
+        }
+    }
+}
