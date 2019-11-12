950b4b6a7299a73e8238694b336728c571081b4c
Clinton Carter <clinton.carter2018@ou.edu>
2019-11-12 12:47:06 -0600

    updated 11/12/2019

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Student newStudent = new Student();
+
+            newStudent.FirstName = TXTFirstName.Text;
+            newStudent.LastName = TXTLastName.Text;
+            newStudent.StudentID = Convert.ToInt32(TXTStudentID.Text);
+            newStudent.FavoriteColor = TXTFavoriteColor.Text;
+
+            LBListBox.Items.Add(newStudent);
+
+            TXTFirstName.Text = string.Empty;
+            TXTLastName.Text = string.Empty;
+            TXTStudentID.Text = string.Empty;
+            TXTFavoriteColor.Text = string.Empty;
+
+
+
+
+
+
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Student
+    {
+        public int StudentID {get; set;}
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public string FavoriteColor { get; set;}
+
+        public Student()
+        {
+            StudentID = 0;
+            FirstName = "";
+            LastName = "";
+            FavoriteColor = "";
+        }
+
+        public override string ToString()
+        {
+          return FirstName + " " + LastName + " " + StudentID + " " + FavoriteColor;
+
+        }
+
+
+
+    }
+}
